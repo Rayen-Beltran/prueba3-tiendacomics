756418f6281594fcbc001c0961b315e50a7e4f30
Rayen B <ra.beltran@duocuc.cl>
2026-06-26 14:25:09 -0400

    eliminar carpeta comics

--- ms_comics/src/main/java/com/comics/comic/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.comics.comic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.comics.comic.model.Categoria;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
-}
--- ms_comics/src/main/java/com/comics/comic/repository/EditorialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.comics.comic.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.comics.comic.model.Editorial;
-
-@Repository
-public interface EditorialRepository extends JpaRepository<Editorial, Integer> {
-}
