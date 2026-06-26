4bbb0e4f18b5c7b8d99ed93fa9580c8b39081b12
unknown <SSDD@POAPCA01LC0420.duoc.acad>
2026-06-26 14:13:18 -0400

    cambio config

--- ms_cliente/src/main/java/com/example/ms_cliente/config/WebClientConfig.java
+++ ms_cliente/src/main/java/com/example/ms_cliente/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.ms_cliente.confi;
+package com.example.ms_cliente.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
