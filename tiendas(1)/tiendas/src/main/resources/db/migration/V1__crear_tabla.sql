d101aa84016d34bbdd464fd42b9b20f57e2773f8
Onyvnn <onyvnn@gmail.com>
2026-06-28 18:42:21 -0400

    intentos

--- tiendas(1)/tiendas/src/main/resources/db/migration/V1__crear_tabla.sql
+++ tiendas(1)/tiendas/src/main/resources/db/migration/V1__crear_tabla.sql
@@ -16,22 +16,21 @@ CREATE TABLE duenos (
 ); INSERT INTO duenos (nombre, apellido) VALUES ('Benjamin', 'Miranda');
 
 CREATE TABLE tiendas (
-   id INT AUTO_INCREMENT PRIMARY KEY,
+   id_tienda INT AUTO_INCREMENT PRIMARY KEY,
    nombre VARCHAR(50) NOT NULL,
    direccion VARCHAR(65) NOT NULL,
    id_duenos INT,
    id_empleados INT,
    id_stock_comics INT,
    CONSTRAINT fk_tiendas_duenos FOREIGN KEY (id_duenos) REFERENCES duenos(id),
-   CONSTRAINT fk_tiendas_empleados FOREIGN KEY (id_empleados) REFERENCES empleados(id),
-   CONSTRAINT fk_tiendas_stock FOREIGN KEY (id_stock_comics) REFERENCES stock_comics(id)
+   CONSTRAINT fk_tiendas_empleados FOREIGN KEY (id_empleados) REFERENCES empleados(id)
 
-); INSERT INTO tiendas (nombre, direccion, id_duenos, id_empleados, id_stock_comics) VALUES ('LOS CRACKS', 'la Farfana 1150',1,1,1);
+); INSERT INTO tiendas (nombre, direccion, id_duenos, id_empleados) VALUES ('LOS CRACKS', 'la Farfana 1150',1,1);
 
 CREATE TABLE stock_comics (
     id_stock INT AUTO_INCREMENT PRIMARY KEY,
     id_tienda INT,
-    id_comic INT NOT NULL, -- Referencia al microservicio de cómics (Sin FK física)
+    id_comic INT NOT NULL, 
     stock INT NOT NULL DEFAULT 0,
-    CONSTRAINT fk_stock_tienda FOREIGN KEY (id_tienda) REFERENCES tienda(id_tienda)
+    CONSTRAINT fk_stock_tienda FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda)
 );
