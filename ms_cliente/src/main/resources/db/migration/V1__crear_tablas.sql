3515d7bb1cbbdf38dd55c3e07b8ddde9877b47f3
Onyvnn <onyvnn@gmail.com>
2026-06-28 23:22:16 -0400

    shet

--- ms_cliente/src/main/resources/db/migration/V1__crear_tablas.sql
+++ ms_cliente/src/main/resources/db/migration/V1__crear_tablas.sql
@@ -10,22 +10,6 @@ CREATE TABLE clientes (
     direccion VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE pago (
-    id_pago INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente INT,
-    id_envio INT,
-    id_comic INT NOT NULL,    
-    id_empleado INT NOT NULL,   
-    id_tienda INT NOT NULL,     
-    descripcion VARCHAR(255),
-    monto_total DECIMAL(10,2) NOT NULL,
-    nombre_tienda VARCHAR(100),
-    direccion_tienda VARCHAR(255),
-    direccion_cliente VARCHAR(255),
-    CONSTRAINT fk_pago_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-    CONSTRAINT fk_pago_envio FOREIGN KEY (id_envio) REFERENCES envio(id_envio)
-);
-
 CREATE TABLE envios (
     id_envio INT AUTO_INCREMENT PRIMARY KEY,
     fechaSalida DATE,
@@ -33,4 +17,20 @@ CREATE TABLE envios (
     tipoEnvio VARCHAR(20),
     sucursal VARCHAR(20),
     id_pago INT
+);
+
+CREATE TABLE pago (
+    id_pago INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente INT,
+    id_envio INT,
+    id_comic INT NOT NULL,
+    id_empleado INT NOT NULL,
+    id_tienda INT NOT NULL,
+    descripcion VARCHAR(255),
+    monto_total DECIMAL(10,2) NOT NULL,
+    nombre_tienda VARCHAR(100),
+    direccion_tienda VARCHAR(255),
+    direccion_cliente VARCHAR(255),
+    CONSTRAINT fk_pago_cliente FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente),
+    CONSTRAINT fk_pago_envio FOREIGN KEY (id_envio) REFERENCES envios(id_envio)
 );
--- tiendas(1)/tiendas/src/main/resources/db/migration/V1__crear_tabla.sql
+++ tiendas(1)/tiendas/src/main/resources/db/migration/V1__crear_tabla.sql
@@ -3,17 +3,19 @@ CREATE TABLE empleados (
    nombre VARCHAR(25) NOT NULL,
    apellido VARCHAR(25) NOT NULL,
    edad INT NOT NULL,
-   rut INT(8) NOT NULL,
+   rut INT NOT NULL,
    dv VARCHAR(1) NOT NULL,
    correo VARCHAR(40) NOT NULL,
    telefono INT(9) NOT NULL
-); INSERT INTO empleados (nombre, apellido, edad, rut, dv, correo, telefono) VALUES ('Jordan','Torres',21, 12332456,'5','JordanTorres@gmail.com', 789654789); 
+);
+INSERT INTO empleados (nombre, apellido, edad, rut, dv, correo, telefono) VALUES ('Jordan','Torres',21, 12332456,'5','JordanTorres@gmail.com', 789654789);
 
 CREATE TABLE duenos (
    id INT AUTO_INCREMENT PRIMARY KEY,
    nombre VARCHAR(20) NOT NULL,
    apellido VARCHAR(20) NOT NULL
-); INSERT INTO duenos (nombre, apellido) VALUES ('Benjamin', 'Miranda');
+);
+INSERT INTO duenos (nombre, apellido) VALUES ('Benjamin', 'Miranda');
 
 CREATE TABLE tiendas (
    id_tienda INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,13 +26,13 @@ CREATE TABLE tiendas (
    id_stock_comics INT,
    CONSTRAINT fk_tiendas_duenos FOREIGN KEY (id_duenos) REFERENCES duenos(id),
    CONSTRAINT fk_tiendas_empleados FOREIGN KEY (id_empleados) REFERENCES empleados(id)
-
-); INSERT INTO tiendas (nombre, direccion, id_duenos, id_empleados) VALUES ('LOS CRACKS', 'la Farfana 1150',1,1);
+);
+INSERT INTO tiendas (nombre, direccion, id_duenos, id_empleados) VALUES ('LOS CRACKS', 'la Farfana 1150',1,1);
 
 CREATE TABLE stock_comics (
     id_stock INT AUTO_INCREMENT PRIMARY KEY,
     id_tienda INT,
-    id_comic INT NOT NULL, 
+    id_comic INT NOT NULL,
     stock INT NOT NULL DEFAULT 0,
     CONSTRAINT fk_stock_tienda FOREIGN KEY (id_tienda) REFERENCES tiendas(id_tienda)
 );
